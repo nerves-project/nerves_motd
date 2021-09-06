c6df919f7574b0dd286acf5da0748f18bf04e901
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2021-09-06 11:45:56 -0400

    Add credo config

---
+++ .credo.exs
@@ -0,0 +1,11 @@
+%{
+  configs: [
+    %{
+      name: "default",
+      checks: [
+        {Credo.Check.Readability.ParenthesesOnZeroArityDefs, parens: true},
+        {Credo.Check.Refactor.Nesting, max_nesting: 4}
+      ]
+    }
+  ]
+}
