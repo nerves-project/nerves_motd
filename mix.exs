30e711f72c5eda2ebde8cde59b7d50b491666f8b
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2021-09-06 12:18:29 -0400

    Improve mix.exs

--- mix.exs
+++ mix.exs
@@ -1,14 +1,25 @@
 defmodule NervesMOTD.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/nerves-project/nerves_motd"
+
   def project do
     [
       app: :nerves_motd,
-      version: "0.1.0",
-      elixir: "~> 1.12",
+      version: @version,
+      elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
+      docs: docs(),
+      description: description(),
+      package: package(),
+      deps: deps(),
+      dialyzer: dialyzer(),
+      preferred_cli_env: %{
+        docs: :docs,
+        "hex.publish": :docs,
+        "hex.build": :docs
+      }
     ]
   end
 
@@ -28,9 +39,37 @@ defmodule NervesMOTD.MixProject do
       {:nerves_runtime, "~> 0.8", optional: true},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.24", only: [:dev], runtime: false},
-      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.25", only: [:dev], runtime: false},
+      {:mix_test_watch, "~> 1.1", only: :dev, runtime: false},
       {:mox, "~> 1.0", only: :test}
+    ]
+  end
+
+  defp docs do
+    [
+      extras: ["README.md", "CHANGELOG.md"],
+      main: "readme",
+      source_ref: "v#{@version}",
+      source_url: @source_url,
+      skip_undefined_reference_warnings_on: ["CHANGELOG.md"]
+    ]
+  end
+
+  defp description do
+    "message of the day (MOTD) for Nerves devices"
+  end
+
+  defp package do
+    [
+      files: ["CHANGELOG.md", "lib", "LICENSE", "mix.exs", "README.md"],
+      licenses: ["Apache-2.0"],
+      links: %{"Github" => @source_url}
+    ]
+  end
+
+  defp dialyzer() do
+    [
+      flags: [:race_conditions, :unmatched_returns, :error_handling, :underspecs]
     ]
   end
 end
