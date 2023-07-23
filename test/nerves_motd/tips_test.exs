1d2354ce8d7e4f6acae46a67acb7f79291425549
Masatoshi N <nishiguchi.masa@gmail.com>
2023-07-23 10:51:56 -0400

    index tips

--- test/nerves_motd/tips_test.exs
+++ test/nerves_motd/tips_test.exs
@@ -3,22 +3,8 @@ defmodule NervesMOTD.TipsTest do
   alias NervesMOTD.Tips
   doctest NervesMOTD.Tips
 
-  @extra_tip """
-  There are only two hard things in Computer Science: cache invalidation and naming things.
-  """
-
-  test "all/1 returns a list of default tips" do
-    assert [tip | _] = Tips.all()
-    assert is_binary(tip)
-  end
-
-  test "all/1 with extra tips returns tips that contain extra tips" do
-    tips = Tips.all(extra_tips: [@extra_tip])
-    assert @extra_tip in tips
-  end
-
-  test "random/1 returns a string" do
-    tip = Tips.random(extra_tips: [@extra_tip])
+  test "random/0 returns a string" do
+    assert {:ok, tip} = Tips.random()
     assert is_binary(tip)
   end
 end
