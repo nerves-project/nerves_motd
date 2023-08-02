7cd0be67229496d1cd3c312e832a67c6cddaa7e3
Frank Hunleth <fhunleth@troodon-software.com>
2023-08-02 10:10:52 -0400

    WIP: move tips compiler to mix

--- lib/nerves_motd/tips.ex
+++ lib/nerves_motd/tips.ex
@@ -1,30 +1,31 @@
 defmodule NervesMOTD.Tips do
   @moduledoc false
 
-  # Returns a tuple list of location and byte size of each tip entry in the provided file.
-  index_file = fn filename, divider when is_binary(filename) and is_binary(divider) ->
-    divider_byte_size = byte_size(divider)
+  # # Returns a tuple list of location and byte size of each tip entry in the provided file.
+  # index_file = fn filename, divider when is_binary(filename) and is_binary(divider) ->
+  #   divider_byte_size = byte_size(divider)
 
-    {indices, _} =
-      File.read!(filename)
-      |> String.trim()
-      |> String.split(divider, trim: true)
-      |> Enum.reduce({[], 0}, fn tip, {indices, current_location} ->
-        how_many_bytes = byte_size(tip)
-        next_location = current_location + how_many_bytes + divider_byte_size
-        new_index = {current_location, how_many_bytes}
+  #   {indices, _} =
+  #     File.read!(filename)
+  #     |> String.trim()
+  #     |> String.split(divider, trim: true)
+  #     |> Enum.reduce({[], 0}, fn tip, {indices, current_location} ->
+  #       how_many_bytes = byte_size(tip)
+  #       next_location = current_location + how_many_bytes + divider_byte_size
+  #       new_index = {current_location, how_many_bytes}
 
-        {[new_index | indices], next_location}
-      end)
+  #       {[new_index | indices], next_location}
+  #     end)
 
-    indices
-  end
+  #   indices
+  # end
 
-  @default_tips_file "priv/default_tips.txt"
-  @divider "%%%"
-  @indices Application.app_dir(:nerves_motd, [@default_tips_file]) |> index_file.(@divider)
+  # @default_tips_file "priv/default_tips.txt"
+  # @divider "%%%"
+  # @indices Application.app_dir(:nerves_motd, [@default_tips_file]) |> index_file.(@divider)
 
-  def indices, do: @indices
+  # @indices
+  def indices, do: []
 
   @doc """
   Pick one tip randomly
