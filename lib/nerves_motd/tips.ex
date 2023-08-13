234480fb46743e9a63b734e2745265c747cbcdc0
Frank Hunleth <fhunleth@troodon-software.com>
2023-08-13 09:20:40 -0400

    Modify tips code to use index file

--- lib/nerves_motd/tips.ex
+++ lib/nerves_motd/tips.ex
@@ -1,50 +1,51 @@
 defmodule NervesMOTD.Tips do
   @moduledoc false
 
-  # # Returns a tuple list of location and byte size of each tip entry in the provided file.
-  # index_file = fn filename, divider when is_binary(filename) and is_binary(divider) ->
-  #   divider_byte_size = byte_size(divider)
-
-  #   {indices, _} =
-  #     File.read!(filename)
-  #     |> String.trim()
-  #     |> String.split(divider, trim: true)
-  #     |> Enum.reduce({[], 0}, fn tip, {indices, current_location} ->
-  #       how_many_bytes = byte_size(tip)
-  #       next_location = current_location + how_many_bytes + divider_byte_size
-  #       new_index = {current_location, how_many_bytes}
-
-  #       {[new_index | indices], next_location}
-  #     end)
-
-  #   indices
-  # end
-
-  # @default_tips_file "priv/default_tips.txt"
-  # @divider "%%%"
-  # @indices Application.app_dir(:nerves_motd, [@default_tips_file]) |> index_file.(@divider)
-
-  # @indices
-  def indices, do: []
-
   @doc """
   Pick one tip randomly
   """
   @spec random() :: {:ok, String.t()} | {:error, any}
   def random() do
-    {location, how_many_bytes} = Enum.random(@indices)
-    read_file_at_location(tips_file(), location, how_many_bytes)
+    path = tips_paths() |> Enum.random()
+
+    {offset, len} = load_index(path) |> Enum.random()
+
+    pread(path, offset, len)
   end
 
-  defp tips_file do
-    # this is different from the one at compile time
-    Application.app_dir(:nerves_motd, [@default_tips_file])
+  def tips_paths() do
+    priv_path = Application.app_dir(:nerves_motd, "priv")
+
+    case File.ls(priv_path) do
+      {:ok, paths} ->
+        paths
+        |> Enum.map(&Path.join(priv_path, &1))
+        |> Enum.filter(&tips_path?/1)
+
+      _error ->
+        []
+    end
   end
 
-  defp read_file_at_location(filename, location, how_many_bytes) do
-    with {:ok, io_device} <- File.open(filename, [:read]),
-         {:ok, data} <- :file.pread(io_device, location, how_many_bytes),
+  def tips_path?(path) do
+    Path.extname(path) == "" and
+      File.exists?([path, ".index"])
+  end
+
+  def load_index(path) do
+    index_path = [path, ".index"]
+    File.read!(index_path) |> :erlang.binary_to_term([:safe])
+  end
+
+  def read_tip(path, index, tip_number) do
+    {offset, len} = Enum.at(index, tip_number)
+    pread(path, offset, len)
+  end
+
+  defp pread(path, offset, len) do
+    with {:ok, io_device} <- File.open(path, [:read]),
+         {:ok, data} <- :file.pread(io_device, offset, len),
          :ok <- File.close(io_device),
-         do: {:ok, String.trim(data)}
+         do: {:ok, data}
   end
 end
